65187bde9c2c97a902ec0cf30a9468645ca277f6
Shubham <mauryashubham0416@gmail.com>
2024-03-24 16:10:40 +0530

    SpringBoot Job Application

---
+++ src/main/java/com/skywins/Job/Application/company/CompanyRepository.java
@@ -0,0 +1,6 @@
+package com.skywins.Job.Application.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
---
+++ src/main/java/com/skywins/Job/Application/company/CompanyService.java
@@ -0,0 +1,18 @@
+package com.skywins.Job.Application.company;
+
+import java.util.List;
+
+;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+
+    boolean updateCompany(Company company, Long id);
+    void createCompany(Company company);
+
+    boolean deleteCompanyById(Long id);
+
+    Company getCompanyById(Long id);
+
+}
+
