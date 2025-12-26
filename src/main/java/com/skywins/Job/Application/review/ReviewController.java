c87f418b62ad1440b39ca806a890169bbd07ce18
Shubham Maurya <mauryashubham0416@gmail.com>
2025-12-26 15:49:03 +0530

    Feature to Master : Base API key added (#22)
    * Base API key added
    
    * Base API key added
    
    * Base API key added
    
    ---------
    
    Co-authored-by: Shubham Maurya <shubham.maurya@eshopbox.com>

--- src/main/java/com/skywins/Job/Application/review/ReviewController.java
+++ src/main/java/com/skywins/Job/Application/review/ReviewController.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/companies/{companyId}")
+@RequestMapping("/api/companies/{companyId}")
 public class ReviewController {
   private ReviewService reviewService;
 
