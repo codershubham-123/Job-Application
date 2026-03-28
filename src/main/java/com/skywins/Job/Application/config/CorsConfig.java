1a37a9955fde2533e335b613909684b511f11337
Shubham Maurya <shubham.maurya@eshopbox.com>
2026-03-28 16:14:37 +0530

    code cleanup

--- src/main/java/com/skywins/Job/Application/config/CorsConfig.java
+++ src/main/java/com/skywins/Job/Application/config/CorsConfig.java
@@ -1,6 +1,4 @@
 package com.skywins.Job.Application.config;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
